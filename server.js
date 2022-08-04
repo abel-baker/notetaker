ff8966adedeb5379d4794d4cc9aafb2ca4a2fca8
abel-baker <seth.keating@gmail.com>
2022-08-04 18:02:48 -0400

    POST functionality

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const res = require('express/lib/response');
+const { json } = require('express/lib/response');
 const getNotes = require('./utils/notes');
 const PORT = process.env.PORT || 3001;
 
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
+
+app.use(express.static('public'));
 
 // parse incoming string/array and JSON data
 app.use(express.urlencoded({ extended: true }));
@@ -21,21 +27,57 @@ function readNoteById(noteId, noteArray) {
   console.log(`in-function ${results[0]}`);
   return results[0];
 }
+function findById(id, array) {
+  const result = array.filter(item => item.id == id)[0];
+  return result;
+}
+function writeNote(body, array) {
+  const { title, content } = body;
+  array.push(body);
 
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(array, null, 2)
+  );
 
-app.get("/api/notes/:id", (req, res) => {
-  const noteId = req.params.id;
-  console.log(noteId);
+  return body;
+}
+
+// Public routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+// API routes
+
+app.get("/api/notes", (req, res) => {
   const notes = getNotes();
-  console.log(notes);
 
-  const note = readNoteById(noteId, notes);
-  console.log(note);
-  if (note) {
-    res.json(note);
+  if (notes) {
+    res.json(notes);
   } else {
     res.sendStatus(404);
   }
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  const note = findById(noteId, getNotes());
+
+  res.json(note);
+});
+
+app.post('/api/notes/', (req, res) => {
+  const notes = getNotes();
+  const note = req.body;
+  note.id = notes.length;
+
+  writeNote(note, notes);
+
+  res.json(note);
 });
 
 
